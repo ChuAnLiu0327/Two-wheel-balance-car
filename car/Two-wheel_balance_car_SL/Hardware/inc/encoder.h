86b0ba8f879436342400a38c8c2d096f200c67b0
ChuAnLiu0327 <103369808+ChuAnLiu0327@users.noreply.github.com>
2025-03-26 10:30:31 +0800

    Update encoder.h

--- car/Two-wheel_balance_car_SL/Hardware/inc/encoder.h
+++ car/Two-wheel_balance_car_SL/Hardware/inc/encoder.h
@@ -1,7 +1,7 @@
 #ifndef __ENCODER_H
 #define __ENCODER_H
 
-#include "stm32f10x.h"                  // Device header
+#include "stm32f10x.h"               
 void Encoder_Init(void);
 int16_t Encoder1_Get(void);
 int16_t Encoder2_Get(void);
